e436d6b4ad4fece3c7b7e7c36e7d59f462cf449f
Eldridge Turambi <eturambi95@gmail.com>
2021-10-18 14:06:10 -0600

    Finish Iteration 1-3

--- lib/potluck.rb
+++ lib/potluck.rb
@@ -1,13 +1,40 @@
 class Potluck
   attr_reader :date,
-              :dishes
+              :dishes,
+              :menu
 
   def initialize(date)
     @date = date
     @dishes = []
+    @menu = Hash.new
   end
 
   def add_dish(dish_name)
     @dishes << dish_name
+  end
+
+  def get_all_from_category(category)
+    @dishes.find_all do |dish|
+      dish.category == category
+    end
+  end
+
+  def menu
+    category_menu_array = {
+      :appetizers => (get_all_from_category(:appetizer).map do |dish|
+        dish.name
+      end).sort!,
+      :entres => (get_all_from_category(:entre).map do |dish|
+        dish.name
+      end).sort!,
+      :desserts => (get_all_from_category(:dessert).map do |dish|
+        dish.name
+      end).sort!
+      }
+      category_menu_array
+  end
+
+  def ratio(category)
+
   end
 end
